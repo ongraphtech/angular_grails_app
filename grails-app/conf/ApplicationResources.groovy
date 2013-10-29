f16dad6072906e47422d299d1c4275ee3479df87
anjain <ankur.jain@ongraph.com>
2013-10-29 21:57:39 +0530

    initial commit

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -3,8 +3,6 @@ modules = {
         dependsOn 'jquery'
         resource url:'js/angular/angular.js'
         resource url:'js/angular/angular-resource.js'
-        resource url:'js/application.js'
-        resource url:'js/grails-default.js'
         resource url:'js/scaffolding.js'
         resource url:'js/angular/twitter-bootstrap.js'
         resource url:'js/angular/angular-strap.js'
